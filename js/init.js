411cad81d97f86147d10fefb39264c05363d7122
ifmglassandaluminium <42646872+ifmglassandaluminium@users.noreply.github.com>
2019-02-04 08:52:26 +0200

    First Commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+ $('.materialboxed').materialbox();
+    $('select').formSelect();
+   
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
