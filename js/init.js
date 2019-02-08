437e632059be6d228f6576a2661cded7ababdac7
Bruv Weird <thulani.ncube21@yahoo.com>
2019-02-08 11:29:38 +0200

    Edited About page and Gallery

--- js/init.js
+++ js/init.js
@@ -4,9 +4,12 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.modal').modal();
- $('.materialboxed').materialbox();
+    $('.materialboxed').materialbox();
     $('select').formSelect();
-   
+   $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
